a05fe6f8fb88359ae98392159ad23ea69580b722
Marcos Paulo <163768929+Marcs-paulo@users.noreply.github.com>
2025-01-08 20:43:36 -0300

    Update config.js

--- components/config.js
+++ components/config.js
@@ -1,15 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-// Your Firebase config object
+// Firebase config object (to be replaced by the user with their Firebase project details)
 const firebaseConfig = {
-  apiKey: 'AIzaSyCsmw72rxaVin42Ufln-Cage7LimVWaHuU',
-  authDomain: 'casasmart-ea97c.firebaseapp.com',
-  databaseURL: 'https://casasmart-ea97c-default-rtdb.firebaseio.com',
-  projectId: 'casasmart-ea97c',
-  storageBucket: 'casasmart-ea97c.firebasestorage.app',
-  messagingSenderId: '1072350976846',
-  appId: '1:1072350976846:android:d2d827d94f19ae22e7dc1f'
+  apiKey: 'YOUR_API_KEY_HERE',
+  authDomain: 'YOUR_AUTH_DOMAIN_HERE',
+  databaseURL: 'YOUR_DATABASE_URL_HERE',
+  projectId: 'YOUR_PROJECT_ID_HERE',
+  storageBucket: 'YOUR_STORAGE_BUCKET_HERE',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID_HERE',
+  appId: 'YOUR_APP_ID_HERE'
 };
 
 // Initialize Firebase
